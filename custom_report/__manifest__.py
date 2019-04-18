f4ce27608df70845ca3bfff4656d1a77764c9d53
Abdullah-Al-Mamun <34166497+LataiHinGhuri@users.noreply.github.com>
2019-04-18 10:52:00 +0600

    Add files via upload

---
+++ custom_report/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Custom Report Template',
+    'summary': """Custom Report Template""",
+    'description': """
+        Custom report template. Separate header and footer with custom design.\n
+        Developed by Jeshad Khan for Metamorphosis Ltd.
+    """,
+    'icon': "/custom_report/static/description/icon.png",
+    "images": ["static/description/custom report template.png.png"],
+
+    'version': '10.0.1.0',
+    'author': 'Metamorphosis',
+    'company': 'Metamorphosis Limited',
+    'website': 'metamorphosis.com.bd',
+    'category': 'Report',
+    'depends': ['base', 'report'],
+    'data': [
+        'data/report_paperformat_data.xml',
+        'layout/custom_report_header_footer.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    "license": "OPL-1",
+}
